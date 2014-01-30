48c75aa61f266eeefc608d8d581b6f81c1a355a8
willnet <netwillnet@gmail.com>
2014-01-30 12:10:27 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResqueTest::Application.config.secret_key_base = '261a3fcd67e190f6b590702d07c42e5fb62fa09047299576b8d0471ac4453bcedbc91420adfecc4145616f8b57cc98ee73bba230f0defa2ce8b45908f27dc429'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ResqueTest::Application.config.session_store :cookie_store, key: '_resque_test_session'
